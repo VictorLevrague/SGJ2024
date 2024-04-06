2450d6b3a831e0e73fc2fe85efad736f76aa67c3
reinkaos <104503481+witchyreinkaos@users.noreply.github.com>
2024-04-06 16:59:53 +0200

    1

---
+++ core/gameplay/GameVar.gd
@@ -0,0 +1,33 @@
+extends Node
+
+### Global ###
+"""
+All global var
+"""
+#It's day when the player can lose
+@export var immunity_day = 20
+
+### Input ###
+@export var game_intput = {
+	"light" = 0.0,
+	"dilution" = 0.0,
+	"glucose" = 0.0
+}
+
+
+### Output ###
+@export var game_output = {
+	"algae" = 0.0,
+	"yield_reactor" = 0.0,
+	"glucose" = 0.0
+}
+
+### Time ###
+@export var time = {
+	"day": 0
+}
+
+#func init():
+#	GameVar.time["day"] = 0.0
+	
+
