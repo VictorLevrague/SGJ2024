9c491d560fcb8e851de8b0f2a64671dbae3912b0
Segfault45 <63585892+PantaRhei@users.noreply.github.com>
2024-04-06 08:41:47 +0200

    add gamevar + gamestate
    add gamevar + gamestate

--- Script/GameState.gd
+++ Script/GameState.gd
@@ -3,17 +3,123 @@ extends Node
 func _process(float):
 	update_gui()
 	
+
+"""
+Take value i and return 1 if the type is TYPE_FLOAT
+else return 0
+"""
+func _private_is_float(i) -> int:
+	if typeof(i) == TYPE_FLOAT:
+		return 1
+	else:
+		return 0
+
+"""
+Take value i and return 1 if the type is TYPE_INT
+else return 0
+"""
+func _private_is_int(i) -> int:
+	if typeof(i) == TYPE_INT:
+		return 1
+	else:
+		return 0
+
+"""
+Take a string in input, input is name of var and value 
+the new value of var
+Return 1 if sucess else -1
+"""
+func modify_input(v: String, value) -> int:
+	if _private_is_float(value):
+		match v:
+			"light":
+				print("[*] modify ligh")
+				GameVar.game_intput["light"] = value
+				return 1
+			"dilution":
+				print("[*] modify dilution")
+				GameVar.game_intput["dilution"] = value
+				return 1
+			"glucose":
+				print("[*] modify glucose")
+				GameVar.game_intput["glucose"] = value
+				return 1
+			"algae":
+				print("[*] modify algae")
+				GameVar.game_intput["algae"] = value
+				return 1
+			"yield_reactor":
+				print("[*] modify yield_reactor")
+				GameVar.game_intput["yield_reactor"] = value
+				return 1
+			"glucose":
+				print("[*] modify glucose")
+				GameVar.game_intput["glucose"] = value
+				return 1
+			_:
+				printerr("[-] Error name var input in modify_input")
+				return -1
+	if _private_is_int(value):
+		match v:
+			"day":
+				print("[*] modify day")
+				GameVar.game_intput["day"] = value
+				return 1
+			_:
+				printerr("[-] Error name var input input in modify_input")
+				return -1
+	printerr("[-] Error value is invalid in modify_input")
+	return -1
+
+
 func update_gui():
 	pass
 
-func check_death():
-	pass
+"""
+Check if the playe is death
+Return 1 if player death else 0
+"""
+func check_death() -> int:
+	#Playe wasn't immunity
+	if GameVar.time["day"] >= GameVar.immunity_day:
+		if GameVar.game_output["algae"] <= 0.0:
+			return 1
+		else:
+			return 0
+	return 0
 
 func check_production():
 	pass
 
+"""
+Update gamestate for new day
+"""
 func progress_day():
 	update_output()
+	_private_update_day()
 
+"""
+Calcul the new value of output for new day
+"""
 func update_output():
-	pass
+	#TODO: Place reel equation
+	GameVar.game_output["algae"] += 0.1
+	GameVar.game_output["yield_reactor"] += 0.1
+	GameVar.game_output["glucose"] += 0.1
+	
+
+"""
+Update time game (add 1 to day)
+"""
+func _private_update_day():
+	GameVar.game_time["day"] += 1
+
+"""
+Print all var in terminal 
+"""
+func print_all_vars():
+	print("immunity_day: ", GameVar.immunity_day)
+	print("game_intput: ", GameVar.game_intput)
+	print("game_output: ", GameVar.game_output)
+	print("time: ", GameVar.time)
+
--- Script/GameVar.gd
+++ Script/GameVar.gd
@@ -1,14 +1,33 @@
 extends Node
 
-# This is global var input 
-var game_input = {
-	"light": 0,
-	"dilution": 0,
-	"glucose": 0
+### Global ###
+"""
+All global var
+"""
+#It's day when the player can lose
+@export var immunity_day = 20
+
+### Input ###
+@export var game_intput = {
+	"light" = 0.0,
+	"dilution" = 0.0,
+	"glucose" = 0.0
 }
 
-var game_output = {
-	"microalgae": 0,
-	"bacteria": 0,
-	"production": 0
+
+### Output ###
+@export var game_output = {
+	"algae" = 0.0,
+	"yield_reactor" = 0.0,
+	"glucose" = 0.0
 }
+
+### Time ###
+@export var time = {
+	"day": 0
+}
+
+#func init():
+#	GameVar.time["day"] = 0.0
+	
+
