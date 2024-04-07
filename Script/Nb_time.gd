aca72ca06700eed24a836eea13d6afca143006cd
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2024-04-07 09:21:23 +0200

    Update time scale slider

--- Script/Nb_time.gd
+++ Script/Nb_time.gd
@@ -1,7 +1,7 @@
 extends Label
 
 func _ready():
-    self.text = str(100/24) + "h"
+    self.text = str(1) + "h"
 
 func _on_slidertime_value_changed(value):
-    self.text = str(int(value * 24/100)) + "h"
+    self.text = str(int(value)) + "h"
